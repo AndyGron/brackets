56d523cd2626e60cd9425e4bc65d0e2e377047c2
AndyGron <14lma.hronski.a@pdu.by>
2021-03-15 18:00:51 +0300

    feat:add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrayString = str.split('');
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let numb1 = bracketsConfig[i][0];
+    let numb2 = bracketsConfig[i][1];
+    for (let j = 0; j < str.length; j++) {
+      if (arrayString[j] == numb1 && arrayString[j+1] == numb2) {
+        arrayString.splice(j, 2);
+        i = -1;
+        j = -1;
+      }
+    }
+  }
+  return arrayString == 0 ? true : false;
 }
+
